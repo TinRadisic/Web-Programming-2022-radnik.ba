658cbf0943d0aef412657eacca0717793b02fbe6
Ervin Vladic <erivn.vladic@stu.ibu.edu.ba>
2022-03-16 17:50:03 +0100

    pushing index.php

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 require 'vendor/autoload.php';
 
 Flight::route('/',function() {
-  echo 'Welcome to the official page of radnik.ba';
+  echo 'Welcome to the official page of radnik.ba!';
 });
-
+$servername = "localhost:3307";
+$username = "";
+$password = "";
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=radnikba", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 Flight::start();
 ?>
