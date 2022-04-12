551513fe2a474786866185bfea2e80656020460b
Harun Kunovac <harun.kunovac@stu.ibu.edu.ba>
2022-04-12 23:20:28 +0200

    adding js functions

--- js/job-service.js
+++ js/job-service.js
@@ -16,14 +16,15 @@ var JobService = {
         for(let i = 0; i < data.length; i++){
           html += `
           <div class="col-lg-3">
-            <div class="card">
+            <div class="card" style="margin-top:30px;">
               <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17ff3c8cf14%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17ff3c8cf14%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.19140625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
               <div class="card-body">
                 <h5 class="card-title">`+ data[i].job_name +`</h5>
                 <p class="card-text">`+ data[i].job_description +`</p>
                 <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-primary job-button" onclick="JobService.get(`+data[i].job_id+`)">Uredi</button>
-                  <button type="button" class="btn btn-danger job-button" onclick="JobService.delete(`+data[i].job_id+`)">Obriši</button>
+                  <button type="button" class="btn btn-primary job-button" onclick="JobService.get(`+data[i].id+`)">Uredi</button>
+                  <button type="button" class="btn btn-danger job-button" onclick="JobService.delete(`+data[i].id+`)">Obriši</button>
+                  <button type="button" class="btn btn-outline-primary job-button" onclick="WorkerService.list_by_job_id(`+data[i].id+`)">Svi radnici</button>
                 </div>
               </div>
             </div>
@@ -38,7 +39,7 @@ var JobService = {
       $('.job-button').attr('disabled', true);
       $.get('rest/job/'+job_id, function(data){
         $("#job_name").val(data.job_name);
-        $("#job_id").val(data.job_id);
+        $("#job_id").val(data.id);
         $("#job_description").val(data.job_description);
         $("#exampleModal").modal("show");
         $('.job-button').attr('disabled', false);
---
+++ js/worker-service.js
@@ -0,0 +1,72 @@
+var WorkerService = {
+
+  add: function(worker){
+    $.ajax({
+      url: 'rest/worker',
+      type: 'POST',
+      data: JSON.stringify(worker),
+      contentType: "application/json",
+      dataType: "json",
+      success: function(result) {
+        // append to the list
+        $("#job-workers").append(`<div class="list-group-item job-worker-`+result.id+`">
+          <button class="btn btn-danger btn-sm float-end" onclick="WorkerService.delete(`+result.id+`)">Obriši</button>
+          <p class="list-group-item-text">`+result.worker_name+', '+result.worker_city+', '+result.worker_phone_number+', '+result.worker_email+', '+result.worker_address+`</p>
+        </div>`);
+        toastr.success("Added !");
+      }
+    });
+  },
+
+  list_by_job_id: function(id){
+    $("#job-workers").html('loading ...');
+    $.get("rest/job/"+id+"/worker", function(data) {
+      var html = "";
+      for(let i = 0; i < data.length; i++){
+        html += `<div class="list-group-item job-worker-`+data[i].id+`">
+          <button class="btn btn-danger btn-sm float-end" onclick="WorkerService.delete(`+data[i].id+`)">Obriši</button>
+          <p class="list-group-item-text">`+data[i].worker_name+', '+data[i].worker_city+', '+data[i].worker_phone_number+', '+data[i].worker_email+', '+data[i].worker_address+`</p>
+        </div>`;
+      }
+      $("#job-workers").html(html);
+    });
+
+    // note id populate and form validation
+    $('#add-worker-form input[name="worker_job_id"]').val(id);
+
+    $('#add-worker-form').validate({
+      submitHandler: function(form) {
+        var entity = Object.fromEntries((new FormData(form)).entries());
+        WorkerService.add(entity);
+        $('#add-worker-form input[name="worker_name"]').val("");
+        $('#add-worker-form input[name="worker_email"]').val("");
+        $('#add-worker-form input[name="worker_city"]').val("");
+        $('#add-worker-form input[name="worker_phone_number"]').val("");
+        $('#add-worker-form input[name="worker_address"]').val("");
+        toastr.info("Adding ...");
+      }
+    });
+    $("#workerModal").modal('show');
+  },
+
+  delete: function(id){
+    var old_html = $("#job-workers").html();
+    $('.job-worker-'+id).remove();
+    toastr.info("Deleting in background ...");
+    $.ajax({
+      url: 'rest/worker/'+id,
+      type: 'DELETE',
+      success: function(result) {
+        toastr.success("Deleted !");
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        toastr.error(XMLHttpRequest.responseJSON.message);
+        $("#job-workers").html(old_html);
+        //alert("Status: " + textStatus); alert("Error: " + errorThrown);
+      }
+    });
+  },
+
+
+
+}
